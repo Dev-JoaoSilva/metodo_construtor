7cf8c1f043ed51377972e048206600ffc8c3a807
João Gabriel <joao.silva2714@etec.sp.gov.br>
2023-10-24 15:02:27 +0000

    salvar

--- Carro.java
+++ Carro.java
@@ -6,6 +6,40 @@ public class Carro{
     private int aro;
     private double valorCarro;
 
+    public int getAro() {
+        return aro;
+    }
+    public void setAro(int aro) {
+        this.aro = aro;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public double getValorCarro() {
+        return valorCarro;
+    }
+    public void setValorCarro(double valorCarro) {
+        this.valorCarro = valorCarro;
+    }
     public Carro(){
 
     }
--- Principal.java
+++ Principal.java
@@ -1,6 +1,14 @@
 public class Principal {
     public static void main(String[] args) {
         
+        Carro car = new Carro();
+        Carro carAro = new Carro(17);
+        Carro carTresParametros = new Carro(17,"GTR","G4ERT5");
+        Carro carQuatroParametros = new Carro(17,"GTR","G4ERT5","PRETO");
+
+        System.out.println(carAro.getAro());
+        System.out.println(carTresParametros.getAro()+" " +carTresParametros.getModelo()+" " +carTresParametros.getPlaca());
+        System.out.println(carQuatroParametros.getAro()+" " +carQuatroParametros.getModelo()+" " +carQuatroParametros.getPlaca()+" " +carQuatroParametros.getCor());
     }
     
 }
